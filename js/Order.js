e47f134e2fdb74b5e90d039d2ac3efa1462ebb1f
Dom Batten <dominic.batten@googlemail.com>
2015-12-02 15:17:57 +0000

    added finalising order handing, all tests passing. about to refactor

--- js/Order.js
+++ js/Order.js
@@ -1,14 +1,15 @@
 Order = function(box) {
   this.box = new box;
   this.selections = [];
+  this.optionalExtras = [];
   this.isDiscounted = false;
 };
 
-var PRICE_OPTIONS = [
-  {"A Grade": 0.2, "B Grade": 0.1, "C Grade": 0.05},
-  {"3 colours": 0.2, "2 colours": 0.1, "Black only": 0.05, "No printing": 0.00, "FantasticBoxCo branding": 0.00},
-  {"Handles": 0.1, "Reinforced bottom": 0.05}
-];
+var PRICE_OPTIONS = {
+  "A Grade": 0.2, "B Grade": 0.1, "C Grade": 0.05,
+  "3 colours": 0.2, "2 colours": 0.1, "Black only": 0.05, "No printing": 0.00, "FantasticBoxCo branding": 0.00,
+  "Handles": 0.1, "Reinforced bottom": 0.05
+};
 
 var DISCOUNT = 0.95;
 
@@ -18,14 +19,39 @@ Order.prototype.newOrder = function(length, width, height, quantity) {
 };
 
 Order.prototype.makeSelection = function(selection) {
+  this.errorChecking(selection);
+  if(selection == "FantasticBoxCo branding") { this.isDiscounted = true }
+  if(selection == "Handles" || selection == "Reinforced bottom") {
+    this.optionalExtras.push(selection);
+  } else {
+    return this.selections.push(selection);
+  };
+};
+
+Order.prototype.finaliseOrder = function() {
+  var surfaceArea = this.box.surfaceArea;
+  var total = 0;
+  var optionsTotal = 0;
+  for(var i = 0; i < this.selections.length; i++) {
+    total += surfaceArea * PRICE_OPTIONS[this.selections[i]];
+  };
+  this.unitCost = parseFloat(total.toFixed(2));
+  if(this.optionalExtras.length > 0) {
+    for(var i = 0; i < this.optionalExtras.length; i++) {
+      optionsTotal += this.quantity * PRICE_OPTIONS[this.optionalExtras[i]];
+    };
+  };
+  this.totalCost = this.unitCost * this.quantity + parseFloat(optionsTotal.toFixed(2));
+  if(this.isDiscounted) {
+    this.totalCost = parseFloat((this.totalCost * DISCOUNT).toFixed(2));
+  };
+};
+
+Order.prototype.errorChecking = function(selection) {
   if(selection == "C Grade" && this.box.surfaceArea > 2) {
     throw "Must be less than 2M^2"
-  }
+  };
   if(selection == "Reinforced bottom" && !(this.selections.indexOf("A Grade") > -1)) {
     throw "Only available for A Grade cardboard";
   };
-  if(selection == "FantasticBoxCo branding") { this.isDiscounted = true }
-  return this.selections.push(selection);
 };
-
-
--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -79,20 +79,56 @@ describe("Order", function() {
       order.makeSelection("A Grade");
       order.makeSelection("3 colours");
       order.makeSelection("Handles");
-      expect(order.selections).toEqual(["A Grade", "3 colours", "Handles"]);
+      expect(order.optionalExtras).toEqual(["Handles"]);
     });
 
     it("should be able to add a reinforced bottom", function() {
       order.makeSelection("A Grade");
       order.makeSelection("3 colours");
       order.makeSelection("Reinforced bottom");
-      expect(order.selections).toEqual(["A Grade", "3 colours", "Reinforced bottom"]);
+      expect(order.optionalExtras).toEqual(["Reinforced bottom"]);
     });
 
     it("should not be able to add reinforced bottom unless grade A cardboard is selected", function() {
       order.makeSelection("B Grade");
       order.makeSelection("3 colours");
       expect(function(){order.makeSelection("Reinforced bottom");}).toThrow("Only available for A Grade cardboard");
+    });
+
+  });
+
+  describe("completing an order", function() {
+
+    it("should correctly calculate the unit cost with no optional extras", function() {
+      order.makeSelection("A Grade");
+      order.makeSelection("3 colours");
+      order.finaliseOrder();
+      expect(order.unitCost).toEqual(0.09);
+    });
+
+    it("should correctly calculate the total cost with no optional extras", function() {
+      var order2 = new Order(Box);
+      order2.newOrder(0.1,0.2,0.3,3);
+      order2.makeSelection("A Grade");
+      order2.makeSelection("3 colours");
+      order2.finaliseOrder();
+      expect(order2.totalCost).toEqual(0.27);
+    });
+
+    it("should correctly calculate the total cost with optional extras", function() {
+      order.makeSelection("A Grade");
+      order.makeSelection("3 colours");
+      order.makeSelection("Handles");
+      order.makeSelection("Reinforced bottom");
+      order.finaliseOrder();
+      expect(order.totalCost).toEqual(0.24);
+    });
+
+    it("should correctly calculate the total cost if there is a discount", function() {
+      order.makeSelection("A Grade");
+      order.makeSelection("FantasticBoxCo branding");
+      order.finaliseOrder();
+      expect(order.totalCost).toEqual(0.04);
     });
 
   });
