9dd34c087781d5297582e32db1d49327e7f08c0c
Dom Batten <dominic.batten@googlemail.com>
2015-12-02 11:15:59 +0000

    can receive order of specified quantity. test passing

--- js/Order.js
+++ js/Order.js
@@ -2,6 +2,7 @@ Order = function(box) {
   this.box = new box;
 };
 
-Order.prototype.newOrder = function(length, width, height) {
+Order.prototype.newOrder = function(length, width, height, quantity) {
   this.box.receiveDimensions(length, width, height);
+  this.quantity = quantity;
 };
--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -7,8 +7,13 @@ describe("Order", function() {
   });
 
   it("should be able to receive an order of a box", function() {
-    order.newOrder(1,2,3);
+    order.newOrder(1,2,3,1);
     expect(order.box.dimensions).toEqual([1,2,3]);
+  });
+
+  it("should be able to receive an order of a specified quantity", function() {
+    order.newOrder(1,2,3,4);
+    expect(order.quantity).toEqual(4);
   });
 
 });
